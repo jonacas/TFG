4daf23144166d2c4da5a6c07a21fe273b54c018d
jonacas <al315616@uji.es>
2018-02-06 18:28:22 +0100

    Level1 Movement and shoot
    +Complete level 1 movement and shoot, less the bounds of the camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour {
     public float fireRate = 0.5f;
     public bool left = true;
     public bool right = true;
+    public GameObject bullet;
+    public Transform bulletPos;
 	// Use this for initialization
 	void Start () {
 		
@@ -16,8 +18,9 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        fireRate = fireRate - Time.deltaTime;
         Inputs();
+        
 	}
 
     void Inputs() {
@@ -26,5 +29,13 @@ public class PlayerMovement : MonoBehaviour {
 
         if (right && m_move >= 0) { this.transform.position = this.transform.position + new Vector3(m_move * Time.deltaTime * speed, 0, 0); }
         else if (left && m_move <= 0) { this.transform.position = this.transform.position + new Vector3(m_move * Time.deltaTime * speed, 0, 0); }
+
+        if (Input.GetAxis("Fire1") != 0 && (fireRate < 0)) { Shoot(); }
+    }
+    void Shoot() {
+
+        fireRate = 0.3f;
+        Instantiate(bullet,new Vector3(this.transform.position.x,this.transform.position.y + this.GetComponent<SpriteRenderer>().bounds.size.y/2,0), Quaternion.identity);
+
     }
 }
