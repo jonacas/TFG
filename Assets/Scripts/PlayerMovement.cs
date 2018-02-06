5388b979f4caae59c3edbedba12261652e8b1dde
jonacas <al315616@uji.es>
2018-02-06 16:08:05 +0100

    First Commit
    +Ship movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    public float speed = 3;
+    public float fireRate = 0.5f;
+    public bool left = true;
+    public bool right = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Inputs();
+	}
+
+    void Inputs() {
+
+        float m_move = Input.GetAxis("Horizontal");
+
+        if (right && m_move >= 0) { this.transform.position = this.transform.position + new Vector3(m_move * Time.deltaTime * speed, 0, 0); }
+        else if (left && m_move <= 0) { this.transform.position = this.transform.position + new Vector3(m_move * Time.deltaTime * speed, 0, 0); }
+    }
+}
