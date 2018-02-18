46278f0e1525250824f340d136fb9245e06b1faa
jonacas <al315616@uji.es>
2018-02-18 20:42:09 +0100

    LV4 Movement, and Starts the LV5 movement

---
+++ Assets/Scripts/PlayerMovementLV4.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementLV4 : MonoBehaviour {
+
+
+    public float speed = 5f;
+    bool left = true;
+    bool right = true;
+    bool up = true;
+    bool down = true;
+    Vector3 ScreenBounds;
+    Vector2 PlayerBounds;
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        ScreenBounds = new Vector2(10f, 5.5f);
+        PlayerBounds = this.GetComponent<MeshRenderer>().bounds.size;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        print(ScreenBounds);
+        ReadInputs();
+        MoveBounds();
+
+	}
+
+    void ReadInputs() {
+
+        float m_move = Input.GetAxis("Horizontal");
+        float m_up = Input.GetAxis("Vertical");
+
+        if (right && m_move > 0) { this.gameObject.transform.position = this.transform.position + new Vector3(speed * Time.deltaTime * m_move,0, 0); }
+        else if (left && m_move < 0) { this.gameObject.transform.position = this.transform.position + new Vector3(speed * Time.deltaTime * m_move,0, 0); }
+        if (up && m_up > 0) { this.gameObject.transform.position = this.transform.position + new Vector3(0, speed * Time.deltaTime * m_up, 0); }
+        else if (down && m_up < 0) { this.gameObject.transform.position = this.transform.position + new Vector3(0, speed * Time.deltaTime * m_up, 0); }
+
+    }
+    void MoveBounds()
+    {
+        left = true;
+        right = true;
+        up = true;
+        down = true;
+        if (this.transform.position.x <= (-(ScreenBounds.x) + PlayerBounds.x / 2))
+        {
+            left = false;
+        }
+        else
+        {
+            if (this.transform.position.x >= ((ScreenBounds.x) - PlayerBounds.x / 2))
+            {
+                right = false;
+            }
+        }
+        if (this.transform.position.y <= (-(ScreenBounds.y) + PlayerBounds.y / 2))
+        {
+
+            down = false;
+
+        }
+        else
+        {
+
+            if (this.transform.position.y >= ((ScreenBounds.y) - PlayerBounds.y / 2))
+            {
+
+                up = false;
+
+            }
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovementLV5.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementLV5 : MonoBehaviour {
+
+    public float speed = 5;
+    public float turnSpeed = 5;
+    Rigidbody rbd;
+	// Use this for initialization
+	void Awake () {
+
+        rbd = this.gameObject.GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ReadInputs();
+
+	}
+
+    void ReadInputs() {
+
+        /*this.transform.position = this.transform.position + new Vector3(turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), Time.deltaTime * turnSpeed * Input.GetAxis("Vertical"), Time.deltaTime * speed);
+        this.transform.eulerAngles = this.transform.eulerAngles + new Vector3(turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), Time.deltaTime * turnSpeed * Input.GetAxis("Vertical"), 0);*/
+        rbd.velocity = new Vector3(turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), Time.deltaTime * turnSpeed * Input.GetAxis("Vertical"), Time.deltaTime * speed);
+        this.transform.eulerAngles = this.transform.eulerAngles + new Vector3(turnSpeed * Time.deltaTime * Input.GetAxis("Vertical"), Time.deltaTime * turnSpeed * Input.GetAxis("Horizontal"), 0);
+
+    }
+}
