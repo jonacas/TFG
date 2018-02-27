018335438245c1a30d0385b5e047e43462e65cef
jonacas <al315616@uji.es>
2018-02-27 20:01:49 +0100

    Shoot in LV5
    +Adjust the bullets to move

--- Assets/Scripts/PlayerMovementLV5.cs
+++ Assets/Scripts/PlayerMovementLV5.cs
@@ -6,6 +6,11 @@ public class PlayerMovementLV5 : MonoBehaviour {
 
     public float speed = 5;
     public float turnSpeed = 5;
+    public float fireRate = 0.1f;
+    public GameObject bullet1;
+    public GameObject bullet2;
+    public Transform bulletExit1;
+    public Transform bulletExit2;
     Rigidbody rbd;
 	// Use this for initialization
 	void Awake () {
@@ -17,9 +22,11 @@ public class PlayerMovementLV5 : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        fireRate = fireRate - Time.deltaTime;
         ReadInputs();
+        
 
-	}
+    }
 
     void ReadInputs() {
 
@@ -28,6 +35,15 @@ public class PlayerMovementLV5 : MonoBehaviour {
         //rbd.velocity = new Vector3(turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), Time.deltaTime * turnSpeed * Input.GetAxis("Vertical"), Time.deltaTime * speed);
         this.transform.Translate(new Vector3(0, 0, Time.deltaTime * speed));
         this.transform.eulerAngles = this.transform.eulerAngles + new Vector3(turnSpeed * Time.deltaTime * Input.GetAxis("Vertical"), Time.deltaTime * turnSpeed * Input.GetAxis("Horizontal"), 0);
+        if (Input.GetAxis("Fire1") != 0 && (fireRate < 0)) { Shoot(); }
+
+    }
+    void Shoot()
+    {
+
+        fireRate = 0.1f;
+        Instantiate(bullet1, new Vector3(bulletExit1.position.x, bulletExit1.position.y, bulletExit1.position.z), Quaternion.identity);
+        Instantiate(bullet2, new Vector3(bulletExit2.position.x, bulletExit2.position.y, bulletExit2.position.z), Quaternion.identity);
 
     }
 }
