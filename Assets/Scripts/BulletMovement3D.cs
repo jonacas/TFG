d355c44e302ed9e0fe441f603768f8d8c84b79f4
jonacas <al315616@uji.es>
2018-02-28 19:06:55 +0100

    Level 5 Shoot and Movement Fixed
    +Not finished yet

---
+++ Assets/Scripts/BulletMovement3D.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement3D : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Start () {
+
+        GetComponent<Rigidbody>().AddForce(this.transform.up * 10000);
+
+    }
+}
--- Assets/Scripts/PlayerMovementLV5.cs
+++ Assets/Scripts/PlayerMovementLV5.cs
@@ -6,12 +6,14 @@ public class PlayerMovementLV5 : MonoBehaviour {
 
     public float speed = 5;
     public float turnSpeed = 5;
-    public float fireRate = 0.1f;
+    public float fireRate = 0.2f;
     public GameObject bullet1;
     public GameObject bullet2;
     public Transform bulletExit1;
     public Transform bulletExit2;
+    public float rotationOffset = -25f;
     Rigidbody rbd;
+    int rotationAxis = 0;
 	// Use this for initialization
 	void Awake () {
 
@@ -30,9 +32,27 @@ public class PlayerMovementLV5 : MonoBehaviour {
 
     void ReadInputs() {
 
-        /*this.transform.position = this.transform.position + new Vector3(turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), Time.deltaTime * turnSpeed * Input.GetAxis("Vertical"), Time.deltaTime * speed);
-        this.transform.eulerAngles = this.transform.eulerAngles + new Vector3(turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), Time.deltaTime * turnSpeed * Input.GetAxis("Vertical"), 0);*/
-        //rbd.velocity = new Vector3(turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), Time.deltaTime * turnSpeed * Input.GetAxis("Vertical"), Time.deltaTime * speed);
+        if (Input.GetAxis("Horizontal") > 0.5f && rotationAxis <= 0)
+        {
+
+            rotationAxis = 1;
+            Rotation(rotationAxis);
+
+
+        }
+        else if (Input.GetAxis("Horizontal") < -0.5f && rotationAxis >= 0)
+        {
+
+            rotationAxis = -1;
+            Rotation(rotationAxis);
+
+        }
+        else if(Input.GetAxis("Horizontal") > -0.15f && Input.GetAxis("Horizontal") < 0.15f)
+        {
+            rotationAxis = 0;
+            Rotation(rotationAxis);
+
+        }
         this.transform.Translate(new Vector3(0, 0, Time.deltaTime * speed));
         this.transform.eulerAngles = this.transform.eulerAngles + new Vector3(turnSpeed * Time.deltaTime * Input.GetAxis("Vertical"), Time.deltaTime * turnSpeed * Input.GetAxis("Horizontal"), 0);
         if (Input.GetAxis("Fire1") != 0 && (fireRate < 0)) { Shoot(); }
@@ -41,9 +61,15 @@ public class PlayerMovementLV5 : MonoBehaviour {
     void Shoot()
     {
 
-        fireRate = 0.1f;
-        Instantiate(bullet1, new Vector3(bulletExit1.position.x, bulletExit1.position.y, bulletExit1.position.z), Quaternion.identity);
-        Instantiate(bullet2, new Vector3(bulletExit2.position.x, bulletExit2.position.y, bulletExit2.position.z), Quaternion.identity);
+        fireRate = 0.2f;
+        Instantiate(bullet1, bulletExit1.position, bulletExit1.rotation);
+        Instantiate(bullet2, bulletExit2.position, bulletExit2.rotation);
+
+    }
+    void Rotation(float x)
+    {
+
+        this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, this.transform.eulerAngles.y, x * rotationOffset);
 
     }
 }
