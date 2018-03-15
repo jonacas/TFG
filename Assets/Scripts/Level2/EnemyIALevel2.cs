6659d23bbd2349f1d49b259347ff9c614974cd46
jonacas <al315616@uji.es>
2018-03-15 15:31:11 +0100

    Enemy Level 2 Shoot
    +Enemy level 2 basic shoot, need few changes

---
+++ Assets/Scripts/Level2/EnemyIALevel2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIALevel2 : MonoBehaviour {
+
+
+    public GameObject player;
+    public GameObject bullet;
+    float shootDealay = 0.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        shootDealay = shootDealay - Time.deltaTime;
+       
+        float distance = Mathf.Abs(transform.position.x - player.transform.position.x);
+        print(distance);
+        if (distance < 3 && shootDealay <=0) {
+
+            Shoot();
+            
+        }
+
+
+	}
+
+    void Shoot() {
+
+        shootDealay = 2.5f;
+        Instantiate(bullet, transform.position,Quaternion.identity);
+
+    }
+}
