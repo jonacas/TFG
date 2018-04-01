e46b1fd6188c8013b3b8e5f1b6d079de87b5f5d4
jonacas <al315616@uji.es>
2018-04-01 19:21:09 +0200

    Shoot rotation

---
+++ Assets/Scripts/Level2/BulletDirection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDirection : MonoBehaviour {
+
+    Vector3 playerPosition;
+    // Use this for initialization
+    void Start () {
+
+        /*playerPosition = PlayerMovementLV2.currentInstance.transform.position;
+        float angleZ = Vector2.Angle(new Vector2(this.transform.position.x,this.transform.position.y), new Vector2(playerPosition.x, playerPosition.y));
+        this.transform.LookAt(playerPosition);
+        this.transform.localEulerAngles = new Vector3(0, 0, angleZ);*/
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Level2/EBulletLV2.cs
+++ Assets/Scripts/Level2/EBulletLV2.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class EBulletLV2 : MonoBehaviour {
 
-    float speed = 3;
+    float speed = 5;
     Vector2 ScreenBounds;
     Vector3 playerPosition;
+    Vector3 pathToPlayer;
     // Use this for initialization
     void Awake()
     {
@@ -17,22 +18,24 @@ public class EBulletLV2 : MonoBehaviour {
     {
 
         playerPosition = PlayerMovementLV2.currentInstance.transform.position;
-        float angleZ = 225 + Vector2.Angle(new Vector2(this.transform.position.x,this.transform.position.y), new Vector2(playerPosition.x, playerPosition.y));
-        //this.transform.LookAt(playerPosition);
-        this.transform.localEulerAngles = new Vector3(0,0,angleZ);
+        //float angleZ = Vector2.Angle(new Vector2(this.transform.position.x,this.transform.position.y), new Vector2(playerPosition.x, playerPosition.y));
+        this.transform.LookAt(playerPosition);
+        this.transform.localEulerAngles = new Vector3(0,0,transform.localEulerAngles.z);
+        pathToPlayer = (playerPosition - transform.position)/ Vector3.Distance(playerPosition,transform.position);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Translate(playerPosition * speed*Time.deltaTime);
+        this.transform.Translate(pathToPlayer * speed*Time.deltaTime);
         InWorld();
     }
 
     void InWorld()
     {
 
-        if (this.transform.position.y < -ScreenBounds.y + this.GetComponent<SpriteRenderer>().bounds.size.y / 2)
+        if (this.transform.position.y < -ScreenBounds.y + this.GetComponentInChildren<SpriteRenderer>().bounds.size.y / 2)
         {
 
             Destroy(this.gameObject);
