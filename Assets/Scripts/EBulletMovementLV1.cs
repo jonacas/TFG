d9e8190426f9772a70c4f139a5040154de399dbd
jonacas <al315616@uji.es>
2018-04-22 17:49:57 +0200

    Level 1 Structure finished
    +All in game mechanics implemented

--- Assets/Scripts/EBulletMovementLV1.cs
+++ Assets/Scripts/EBulletMovementLV1.cs
@@ -32,7 +32,7 @@ public class EBulletMovementLV1 : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Cover" )
         {
 
             Destroy(collision.gameObject);
