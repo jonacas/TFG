060c69aab3e6d99484e1caa12b32c65d5847d785
jonacas <al315616@uji.es>
2018-05-23 00:18:20 +0200

    Bullet Hell System Basic

---
+++ Assets/Scripts/Level3/BulletLv3.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLv3 : MonoBehaviour {
+
+
+    public float speed = 3;
+    public float rotation = 30;
+	// Use this for initialization
+	void Start () {
+
+        transform.Rotate(Vector3.forward * rotation);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (this.gameObject.name.Contains("Clone"))
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+	}
+}
---
+++ Assets/Scripts/Level3/EnemyLv3Shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLv3Shoot : MonoBehaviour {
+
+    public Transform[] bullets;
+	// Use this for initialization
+	void Start () {
+
+        bullets = this.GetComponentsInChildren<Transform>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Z)) {
+
+            foreach (Transform bullet in bullets)
+            {
+                if (bullet.tag == "bullet") {
+                    Instantiate(bullet.gameObject, this.transform.position, Quaternion.identity);
+                }
+                
+            }
+
+        }
+        
+		
+	}
+}
